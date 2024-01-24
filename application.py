ab855e64652e63ca9ed996c42fc611a302c76465
diliprandhi <dileep89851@gmail.com>
2024-01-25 00:23:34 +0530

    Deployment config for AWS

--- application.py
+++ application.py
@@ -6,11 +6,11 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
 app=application
-@app.route('/')
+'''@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')'''
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
